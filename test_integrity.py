f5fd111760dfc4308714516fd40a2d57c1cbde6a
Ted Wong <WongMingYin@gmail.com>
2022-09-05 20:33:59 +1000

    Delete test_integrity.py

--- test_integrity.py
+++
@@ -1,8 +0,0 @@
-import integrity
-
-
-def test_1():
-    x = integrity.run("data/simulated.bam")
-    assert(x["bed_path"] == "data/mrna_target.bed")
-    assert(x["intersect_path"] == ".mana/simulated.bam_integrity_intersected.bam")
-    assert(x["flagstat_path"] == ".mana/simulated.bam_integrity_flagstat.txt")
