1b929d49a32d7e0c56d1f1f4f7f68793409ca1aa
Ted Wong <tedwong@smallchess.com>
2022-09-21 19:10:02 +1000

    ...

--- mana.py
+++ mana.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     parser.add_argument("-b", help="Input BAM file containing ONT reads aligned to plasmid sequence", required=True)
     parser.add_argument("-f", help="Input reference FASTA file of the plasmid sequence used to generate mRNA", required=True)
     parser.add_argument("-ecoli", help="Input FASTA file of the bacterium used for plasmid propagation. Default will be E.coli K12 ASM584v2.", required=False)
-    parser.add_argument("-p1", help="Start coordinate of mRNA", required=False)
-    parser.add_argument("-p2", help="Last coordinate of mRNA before PolyA tract", required=False)
+    parser.add_argument("-p1", help="Start coordinate of mRNA", required=True)
+    parser.add_argument("-p2", help="Last coordinate of mRNA before PolyA tract", required=True)
     args = parser.parse_args()
 
     if not args.mrna and not args.plasmid:
@@ -48,7 +48,9 @@ if __name__ == '__main__':
             raise Exception(p1 + " is not a number")
         elif not p2.isdigit():
             raise Exception(p2 + " is not a number")
-        elif p1 >= p2:
+        p1 = int(p1)
+        p2 = int(p2)
+        if p1 >= p2:
             raise Exception("p2 must be greater than p1")
         with open("/tmp/mrna_target.bed", "w") as w:
             w.write("2021-8\t" + str(p1) + "\t" + str(p2))
