93d1ec75e06e5eda05472b0a84d8a7c839ad2c63
Your Name <you@example.com>
2022-11-21 01:21:24 +1100

    ...

--- mana.py
+++ mana.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     parser.add_argument("-b", help="Input BAM file containing ONT reads aligned to plasmid sequence", required=True)
     parser.add_argument("-f", help="Input reference FASTA file of the plasmid sequence used to generate mRNA", required=True)
     parser.add_argument("-ecoli", help="Input FASTA file of the bacterium used for plasmid propagation. Default will be E.coli K12 ASM584v2.", required=False)
-    parser.add_argument("-p1", help="Start coordinate of mRNA", required=True)
-    parser.add_argument("-p2", help="Last coordinate of mRNA before PolyA tract", required=True)
+    parser.add_argument("-p1", help="Start coordinate of mRNA", required=False)
+    parser.add_argument("-p2", help="Last coordinate of mRNA before PolyA tract", required=False)
     args = parser.parse_args()
 
     if not args.mrna and not args.plasmid:
