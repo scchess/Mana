47ba405737ce946a155cab43698aa8bfb9c268b2
Your Name <you@example.com>
2022-11-23 05:48:08 +1100

    ...

--- test_bcftools.py
+++ test_bcftools.py
@@ -1,5 +1,10 @@
 import bcftools
+import subprocess
 
 
 def test_1():
-    bcftools.run("data/GCF_000005845.2_ASM584v2_genomic.fna", "data/simulated.bam")
+    x = bcftools.run("plasmid", "data/2022_17_483289_F8.fasta", "data/mrna17_plasmid_allpassedreads_sorted.bam")
+    assert(x["consensus_path"] == ".mana/mrna17_plasmid_allpassedreads_sorted.bam_consensus.fa")
+    assert(x["variants_path"] == ".mana/mrna17_plasmid_allpassedreads_sorted.bam_variants.vcf.gz")
+    x = subprocess.run(["md5sum", ".mana/mrna17_plasmid_allpassedreads_sorted.bam_consensus.fa"], capture_output=True, text=True).stdout
+    assert "358d717cfc38ea954e028aee466916b3" in x
